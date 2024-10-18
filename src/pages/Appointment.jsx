ed77abeb64bb38e883e8965146ddc4ed45afdb30
kidusm001 <kidusmesfin27@gmail.com>
2024-10-18 04:33:54 +0300

    Appointment Page Added

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -1,7 +1,19 @@
 // import React from 'react'
 
+import AppointmentForm from "../components/AppointmentForm";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Topbar from "../components/Topbar";
+
 const Appointment = () => {
-  return <div></div>;
+  return (
+    <>
+      <Topbar />
+      <Navbar />
+      <AppointmentForm />
+      <Footer />
+    </>
+  );
 };
 
 export default Appointment;
